8fe48d3e9b891088eca9abe609b8d697677d9b17
Rabin R <rabinramesh777.rr@gmail.com>
2025-06-26 15:38:21 +0530

    setting up the web-scraper

---
+++ web_scraping.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+
+link = "https://www.bbc.com/"
+
+source = requests.get(link).text
+
+soup = BeautifulSoup(source, "html5lib")
+titles = soup.find_all("h2")
+
+"""for title in titles:
+    print(title.text)"""
+
+with open("news.txt", "w") as file:
+    for title in titles:
+        file.write(title.get_text(strip=True) + "\n")
