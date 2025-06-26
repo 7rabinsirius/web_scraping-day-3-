1366623b80979e84adb90aba946c24b332fa8e96
Rabin R <rabinramesh777.rr@gmail.com>
2025-06-26 16:17:36 +0530

    added error handling

--- web_scraping.py
+++ web_scraping.py
@@ -2,15 +2,18 @@ from bs4 import BeautifulSoup
 import requests
 
 link = "https://www.bbc.com/"
-
-source = requests.get(link).text
-
-soup = BeautifulSoup(source, "html5lib")
-titles = soup.find_all("h2", attrs={"data-testid": "card-headline"})
+try:
+    source = requests.get(link).text
+    soup = BeautifulSoup(source, "html5lib")
+    titles = soup.find_all("h2", attrs={"data-testid": "card-headline"})
 
 """for title in titles:
     print(title.text)"""
+    with open("news.txt", "w") as file:
 
-with open("news.txt", "w") as file:
-    for title in titles:
-        file.write(title.get_text(strip=True) + "\n")
+        for title in titles:
+            file.write(title.get_text(strip=True) + "\n")
+
+except requests.exceptions.RequestException as e:
+    print(f"Error fetching HTML: {e}")
+
